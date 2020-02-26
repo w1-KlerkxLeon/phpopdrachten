2c456b6ed33fced5df4f61c7c74e91aaff07b136
Leon <leon.klerkx@edu-kw1c.nl>
2020-02-26 15:02:21 +0100

    Add aanpassing webdesign

---
+++ Periode 3/Hoofdstuk3/Opdracht_3.3/opdracht_3.3.php
@@ -0,0 +1,18 @@
+<?php
+    //Header inladen
+    include "../../Includes/header.php";
+    //Menu inladen
+    include "../../Includes/menu.php";
+?>
+
+<main id="wrapper">
+    <!--Titel-->
+    <h2>
+        Uitwerkingen
+    </h2>
+
+</main>
+<?php
+    //Footer inladen
+    include "../../Includes/footer.php";
+?>
