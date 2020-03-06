f71fe15013f1debee595de39ba3816de7e8a9fb6
Leon <leon.klerkx@edu-kw1c.nl>
2020-03-06 09:13:09 +0100

    Opdracht 4.1

---
+++ Periode 3/Includes/footer.php
@@ -0,0 +1,50 @@
+<?php
+/*
+ *  User:   Leon Klerkx
+ *  Date:   19-2-2020
+ *  Time:   14:40 uur
+ *  File:   footer.php
+ */
+
+    //variabelen.php inladen (opdracht_3.3 taak 6)
+    include "../../Hoofdstuk3/Opdracht_3.3/variabelen.php";
+?>
+
+<!--Elementen afsluiten-->
+    </main>
+    <!--Footer-->
+    <footer>
+        <span>&copy; <?php echo $name;?> </span>
+        <?php
+        //Taak 6 opdracht 2
+        echo $year . "&nbsp;";
+
+        //Opdracht 4.1 tijdzone instellen taak 1 en 2
+        date_default_timezone_set("Europe/Amsterdam");
+        //variabele $uur declareren en initialiseren
+        $uur = date("H");
+
+        //Opdracht 4.1 taak 3 if/else
+        //Tussen 0 en 5 uur
+        if($uur >= 0 && $uur < 5)
+        {
+            echo "Goedennacht";
+        }
+        //Tussen 5 en 12 uur
+        elseif($uur >= 5 &&  $uur < 12)
+        {
+            echo "Goedenochtend";
+        }
+        //Tussen 12 en 17 uur
+        elseif($uur >= 12 && $uur < 17)
+        {
+            echo "Goedenmiddag";
+        }
+        //Tussen 17 en 24 uur
+        elseif($uur >= 17 && $uur < 24)
+        {
+            echo "Goedenavond";
+        }
+        ?>
+    </footer>
+</body>
