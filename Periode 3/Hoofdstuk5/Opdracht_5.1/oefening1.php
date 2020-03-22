0047909ae8de27f852b83e89b1692a2aef24d213
Leon <leon.klerkx@edu-kw1c.nl>
2020-03-22 17:44:44 +0100

    Hoofdstuk 5 oefening 1 (opdracht 5.1)

---
+++ Periode 3/Hoofdstuk5/Opdracht_5.1/oefening1.php
@@ -0,0 +1,17 @@
+<?php
+/*
+ *  User:   Leon Klerkx
+ *  Date:   19-3-2020
+ *  Time:   21:00 uur
+ *  File:   opdracht_5.1.php
+ */
+?>
+
+<?php
+   echo "<form action='form_data_oefening1.php' method='get'>";
+   echo "<label>Naam: </label>";
+   echo "<input name='nameExercise' type='text'>";
+   //echo "<input name='cityExcercise' type='text'>";
+   echo "<input type='submit' name='verzend' value='verzend'>";
+   echo "</form>";
+?>
