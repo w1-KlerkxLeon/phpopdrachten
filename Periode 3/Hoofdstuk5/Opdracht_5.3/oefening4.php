60f720d8dd0362e089e9eceec315762a20744efc
Leon <leon.klerkx@edu-kw1c.nl>
2020-04-05 19:17:54 +0200

    Oefening 4 (opdracht 5.3)

---
+++ Periode 3/Hoofdstuk5/Opdracht_5.3/oefening4.php
@@ -0,0 +1,11 @@
+<?php
+    include "../../Includes/functions.php";
+?>
+
+<?php
+    //Function 'hallo wereld' tonen
+    echo helloWorld();
+
+    //Function 'en tot ziens' tonen
+    echo getGoodbye();
+?>
